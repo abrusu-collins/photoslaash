ea354e1b303401f068a05fd2e52d45af2c9d56f4
abrusu-collins <snillochemingson@gmail.com>
2023-04-02 13:57:57 +0000

    neat

--- src/components/landing_page/Testimonies.jsx
+++ src/components/landing_page/Testimonies.jsx
@@ -2,68 +2,71 @@ import { AiFillStar } from "react-icons/ai";
 function Testimonies() {
   return (
     <div className="testimonies">
-      <div className="grid1">
-        <p className="testimonial-title">Best app ever</p>
-        <p className="testimonial-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-          quasi voluptatum quas sint deleniti recusandae laboriosam voluptatibus
-          fugit provident at, quibusdam, tempora ut adipisci et optio veritatis
-          nulla earum?
-        </p>
-        <div className="stars">
-          <AiFillStar color="#0362fc" size={25} />
-          <AiFillStar color="#0362fc" size={25} />
-          <AiFillStar color="#0362fc" size={25} />
-          <AiFillStar color="#0362fc" size={25} />
-          <AiFillStar color="#0362fc" size={25} />
+        <p className="users">What our users say about us</p>
+      <div className="testimonials-inner">
+        <div className="grid1">
+          <p className="testimonial-title">Best app ever</p>
+          <p className="testimonial-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
+            quasi voluptatum quas sint deleniti recusandae laboriosam
+            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
+            optio veritatis nulla earum?
+          </p>
+          <div className="stars">
+            <AiFillStar color="#0362fc" size={25} />
+            <AiFillStar color="#0362fc" size={25} />
+            <AiFillStar color="#0362fc" size={25} />
+            <AiFillStar color="#0362fc" size={25} />
+            <AiFillStar color="#0362fc" size={25} />
+          </div>
         </div>
-      </div>
-      <div className="grid2">
-        <p className="testimonial-title">Best app ever</p>
-        <p className="testimonial-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-          quasi voluptatum quas sint deleniti recusandae laboriosam voluptatibus
-          fugit provident at, quibusdam, tempora ut adipisci et optio veritatis
-          nulla earum?
-        </p>
-        <div className="stars">
-          <AiFillStar color="#0fcfeff" size={25} />
-          <AiFillStar color="#0fcfeff" size={25} />
-          <AiFillStar color="#0fcfeff" size={25} />
-          <AiFillStar color="#0fcfeff" size={25} />
-          <AiFillStar color="#0fcfeff" size={25} />
+        <div className="grid2">
+          <p className="testimonial-title">Best app ever</p>
+          <p className="testimonial-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
+            quasi voluptatum quas sint deleniti recusandae laboriosam
+            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
+            optio veritatis nulla earum?
+          </p>
+          <div className="stars">
+            <AiFillStar color="#0fcfeff" size={25} />
+            <AiFillStar color="#0fcfeff" size={25} />
+            <AiFillStar color="#0fcfeff" size={25} />
+            <AiFillStar color="#0fcfeff" size={25} />
+            <AiFillStar color="#0fcfeff" size={25} />
+          </div>
         </div>
-      </div>
-      <div className="grid3">
-        <p className="testimonial-title">Best app ever</p>
-        <p className="testimonial-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-          quasi voluptatum quas sint deleniti recusandae laboriosam voluptatibus
-          fugit provident at, quibusdam, tempora ut adipisci et optio veritatis
-          nulla earum?
-        </p>
-        <div className="stars">
-          <AiFillStar color="#fcfeff" size={25} />
-          <AiFillStar color="#fcfeff" size={25} />
-          <AiFillStar color="#fcfeff" size={25} />
-          <AiFillStar color="#fcfeff" size={25} />
-          <AiFillStar color="#fcfeff" size={25} />
+        <div className="grid3">
+          <p className="testimonial-title">Best app ever</p>
+          <p className="testimonial-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
+            quasi voluptatum quas sint deleniti recusandae laboriosam
+            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
+            optio veritatis nulla earum?
+          </p>
+          <div className="stars">
+            <AiFillStar color="#fcfeff" size={25} />
+            <AiFillStar color="#fcfeff" size={25} />
+            <AiFillStar color="#fcfeff" size={25} />
+            <AiFillStar color="#fcfeff" size={25} />
+            <AiFillStar color="#fcfeff" size={25} />
+          </div>
         </div>
-      </div>
-      <div className="grid4">
-        <p className="testimonial-title">Best app ever</p>
-        <p className="testimonial-text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-          quasi voluptatum quas sint deleniti recusandae laboriosam voluptatibus
-          fugit provident at, quibusdam, tempora ut adipisci et optio veritatis
-          nulla earum?
-        </p>
-        <div className="stars">
-          <AiFillStar color="#000000" size={25} />
-          <AiFillStar color="#000000" size={25} />
-          <AiFillStar color="#000000" size={25} />
-          <AiFillStar color="#000000" size={25} />
-          <AiFillStar color="#000000" size={25} />
+        <div className="grid4">
+          <p className="testimonial-title">Best app ever</p>
+          <p className="testimonial-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
+            quasi voluptatum quas sint deleniti recusandae laboriosam
+            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
+            optio veritatis nulla earum?
+          </p>
+          <div className="stars">
+            <AiFillStar color="#000000" size={25} />
+            <AiFillStar color="#000000" size={25} />
+            <AiFillStar color="#000000" size={25} />
+            <AiFillStar color="#000000" size={25} />
+            <AiFillStar color="#000000" size={25} />
+          </div>
         </div>
       </div>
     </div>
