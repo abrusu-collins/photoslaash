0448417b95c4bf7bd5f7c9d0cc774aa1218d02c6
abrusu-collins <snillochemingson@gmail.com>
2023-06-18 17:45:47 +0000

    copywriting

--- src/components/landing_page/Testimonies.jsx
+++ src/components/landing_page/Testimonies.jsx
@@ -2,15 +2,15 @@ import { AiFillStar } from "react-icons/ai";
 function Testimonies() {
   return (
     <div className="testimonies">
-        <p className="users">What our users say about us</p>
+      <p className="users">What our users say about us</p>
       <div className="testimonials-inner">
         <div className="grid1">
           <p className="testimonial-title">Best app ever</p>
           <p className="testimonial-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-            quasi voluptatum quas sint deleniti recusandae laboriosam
-            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
-            optio veritatis nulla earum?
+            Wow, this image compression app is a game-changer! I was able to
+            compress a batch of 100 high-resolution images in just a few
+            minutes, without any loss in quality. It saved me so much storage
+            space and made sharing them a breeze.
           </p>
           <div className="stars">
             <AiFillStar color="#0362fc" size={25} />
@@ -23,10 +23,11 @@ function Testimonies() {
         <div className="grid2">
           <p className="testimonial-title">Best app ever</p>
           <p className="testimonial-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-            quasi voluptatum quas sint deleniti recusandae laboriosam
-            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
-            optio veritatis nulla earum?
+            I've tried several image compression tools, but none compare to this
+            app. It handled my diverse range of images flawlessly, from
+            professional photographs to casual snapshots. The compressed images
+            look fantastic, and I can't believe how much faster they load on my
+            website now.
           </p>
           <div className="stars">
             <AiFillStar color="#0fcfeff" size={25} />
@@ -39,10 +40,11 @@ function Testimonies() {
         <div className="grid3">
           <p className="testimonial-title">Best app ever</p>
           <p className="testimonial-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-            quasi voluptatum quas sint deleniti recusandae laboriosam
-            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
-            optio veritatis nulla earum?
+            I love how user-friendly this app is! Uploading and compressing
+            multiple images at once is a breeze, and the results are impressive.
+            It's made managing my photo library so much easier, allowing me to
+            free up space on my devices and enjoy seamless sharing across all my
+            social media platforms
           </p>
           <div className="stars">
             <AiFillStar color="#fcfeff" size={25} />
@@ -55,10 +57,10 @@ function Testimonies() {
         <div className="grid4">
           <p className="testimonial-title">Best app ever</p>
           <p className="testimonial-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus est
-            quasi voluptatum quas sint deleniti recusandae laboriosam
-            voluptatibus fugit provident at, quibusdam, tempora ut adipisci et
-            optio veritatis nulla earum?
+            I'm amazed at how this app strikes the perfect balance between file
+            size reduction and image quality. It managed to significantly shrink
+            my images while retaining the vibrant colors and sharp details. It's
+            become an essential tool for my online business.
           </p>
           <div className="stars">
             <AiFillStar color="#000000" size={25} />
